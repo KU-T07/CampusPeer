b6c37459b67a64b55d93f3680655304e8fc735c1
GeunNyeong Jeong <kkackit@gmail.com>
2025-06-19 23:03:44 +0900

    main1

--- app/src/main/java/com/example/campuspeer/uicomponent/MainScreen.kt
+++ app/src/main/java/com/example/campuspeer/uicomponent/MainScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
@@ -23,8 +24,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModelStoreOwner
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.currentBackStackEntryAsState
@@ -34,6 +39,7 @@ import com.example.campuspeer.itemBoard.PostItemViewModel
 import com.example.campuspeer.model.Routes
 import com.example.campuspeer.navigation.BottomNavigationBar
 import com.example.campuspeer.navigation.NaviGraph
+import com.example.campuspeer.ui.theme.Pretendard
 import com.example.campuspeer.viewmodel.EmailAuthViewModel
 
 @Composable
@@ -81,41 +87,54 @@ fun MainScreen(modifier: Modifier = Modifier) {
         }
     ) {
 
-        CompositionLocalProvider(
-            LocalNavGraphViewModelStoreOwner provides navStoreOwner
-        ) {
-            Scaffold(
-                topBar = {
-                    if (currentRoute.isRoot)
-                        TopAppBar(
-                            title = { Text(text = currentRoute.route) }
+    CompositionLocalProvider(
+        LocalNavGraphViewModelStoreOwner provides navStoreOwner
+    ) {
+        Scaffold(
+            topBar = {
+                if (currentRoute.isRoot)
+                    TopAppBar(
+                        title = {
+                            Text(
+                                text = currentRoute.route, style = TextStyle(
+                                    fontFamily = Pretendard,
+                                    fontWeight = FontWeight.Bold,
+                                    fontSize = 32.sp
+                                )
+                            )
+                        },
+
+                        colors = TopAppBarDefaults.topAppBarColors(
+                            containerColor = Color(0xFF85C687),
+                            titleContentColor = Color.White     // 타이틀 색
                         )
-                },
-                bottomBar = {
-                    if (currentRoute.isRoot)
-                        BottomNavigationBar(navController)
-                },
-                floatingActionButton = {
-                    if (currentRoute == Routes.PostItemList)
-                        FloatingActionButton(onClick = {
-                            navController.navigate(Routes.PostItemCreate.route)
-                        }) {
-                            Icon(imageVector = Icons.Default.Add, contentDescription = "")
-                        }
-                }
-            ) { contentPadding ->
-                Column(modifier = Modifier.padding(contentPadding)) {
-                    NaviGraph(
-                        navController = navController,
-                        currentUserId = viewModel.getCurrentUserId().toString(),
-                        startRoute = Routes.PostItemList.route,
-                        allPosts = posts
                     )
-                    Text(text = viewModel.getCurrentUserId().toString())
-                }
+            },
+            bottomBar = {
+                if (currentRoute.isRoot)
+                    BottomNavigationBar(navController)
+            },
+            floatingActionButton = {
+                if (currentRoute == Routes.PostItemList)
+                    FloatingActionButton(onClick = {
+                        navController.navigate(Routes.PostItemCreate.route)
+                    }) {
+                        Icon(imageVector = Icons.Default.Add, contentDescription = "")
+                    }
+            }
+        ) { contentPadding ->
+            Column(modifier = Modifier.padding(contentPadding)) {
+                NaviGraph(
+                    navController = navController,
+                    currentUserId = viewModel.getCurrentUserId().toString(),
+                    startRoute = Routes.PostItemList.route,
+                    allPosts = posts
+                )
+                Text(text = viewModel.getCurrentUserId().toString())
             }
         }
     }
+}
 
 }
 @Preview(showBackground = true)
